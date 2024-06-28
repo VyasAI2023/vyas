904e57ac62036528b7bc7debf9bdd3e27112bf06
Vansh Seth <111755254+vansh-seth@users.noreply.github.com>
2024-06-28 16:01:24 +0530

    Update Blog.jsx

--- src/components/company/Blog.jsx
+++ src/components/company/Blog.jsx
@@ -13,7 +13,7 @@ export const Blog = () => {
   const navigate = useNavigate();
 
   const handleSelect = () => {
-    navigate('/');
+    navigate('/company/blog1');
   };
 
   return (
@@ -24,37 +24,6 @@ export const Blog = () => {
         <div className='underline'></div><br/><br/>
       </div>
       <div className='card-container' style={{ display: 'flex', gap: '20px', justifyContent: 'center'}}>
-      <Card sx={{ maxWidth: 345,borderRadius: '10%' }}>
-          <CardMedia
-            component="img"
-            height="140"
-            image="https://img.freepik.com/free-photo/online-message-blog-chat-communication-envelop-graphic-icon-concept_53876-139717.jpg"
-            alt="Model Image"
-          />
-        <div class="flex justify-between space-x-6 mb-3 text-black mt-4 mx-4">
-            <time datetime="2024-01-06 21:52:42" class="text-sm">06 Jan</time>
-            <div class="border-b grow relative -top-2"></div>
-            <a class="text-sm" href="">Admin</a>
-        </div>
-
-        <CardContent sx={{ paddingTop: 0 }}>
-            <Typography gutterBottom variant="h5" component="div">
-                Blog Name:
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-                context of the blog
-            </Typography>
-        </CardContent>
-          <CardActions sx={{ justifyContent: 'center' }}>
-          <a class="flex items-center mt-auto text-[13px] text-black" href="/company/blog1">
-            Read More
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M9 6l6 6l-6 6"></path>
-            </svg>
-        </a>
-          </CardActions>
-        </Card>
         <Card sx={{ maxWidth: 345,borderRadius: '10%' }}>
           <CardMedia
             component="img"
@@ -62,30 +31,27 @@ export const Blog = () => {
             image="https://img.freepik.com/free-photo/online-message-blog-chat-communication-envelop-graphic-icon-concept_53876-139717.jpg"
             alt="Model Image"
           />
-        <div class="flex justify-between space-x-6 mb-3 text-black mt-4 mx-4">
-            <time datetime="2024-01-06 21:52:42" class="text-sm">06 Jan</time>
-            <div class="border-b grow relative -top-2"></div>
-            <a class="text-sm" href="">Admin</a>
-        </div>
+          <div className="flex justify-between space-x-6 mb-3 text-black mt-4 mx-4">
+            <time dateTime="2024-01-06 21:52:42" className="text-sm">06 Jan</time>
+            <div className="border-b grow relative -top-2"></div>
+            <a className="text-sm" href="">Admin</a>
+          </div>
 
-        <CardContent sx={{ paddingTop: 0 }}>
+          <CardContent sx={{ paddingTop: 0 }}>
             <Typography gutterBottom variant="h5" component="div">
-                Blog Name:
+              Blog Name:
             </Typography>
             <Typography variant="body2" color="text.secondary">
-                context of the blog
+              context of the blog
             </Typography>
-        </CardContent>
+          </CardContent>
           <CardActions sx={{ justifyContent: 'center' }}>
-          <a class="flex items-center mt-auto text-[13px] text-black" href="/company/blog1">
-            Read More
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M9 6l6 6l-6 6"></path>
-            </svg>
-        </a>
+            <Button variant="contained" color="primary" onClick={handleSelect}>
+              Read More
+            </Button>
           </CardActions>
         </Card>
+
         <Card sx={{ maxWidth: 345,borderRadius: '10%' }}>
           <CardMedia
             component="img"
@@ -93,33 +59,56 @@ export const Blog = () => {
             image="https://img.freepik.com/free-photo/online-message-blog-chat-communication-envelop-graphic-icon-concept_53876-139717.jpg"
             alt="Model Image"
           />
-        <div class="flex justify-between space-x-6 mb-3 text-black mt-4 mx-4">
-            <time datetime="2024-01-06 21:52:42" class="text-sm">06 Jan</time>
-            <div class="border-b grow relative -top-2"></div>
-            <a class="text-sm" href="">Admin</a>
-        </div>
+          <div className="flex justify-between space-x-6 mb-3 text-black mt-4 mx-4">
+            <time dateTime="2024-01-06 21:52:42" className="text-sm">06 Jan</time>
+            <div className="border-b grow relative -top-2"></div>
+            <a className="text-sm" href="">Admin</a>
+          </div>
 
-        <CardContent sx={{ paddingTop: 0 }}>
+          <CardContent sx={{ paddingTop: 0 }}>
             <Typography gutterBottom variant="h5" component="div">
-                Blog Name:
+              Blog Name:
             </Typography>
             <Typography variant="body2" color="text.secondary">
-                context of the blog
+              context of the blog
             </Typography>
-        </CardContent>
+          </CardContent>
           <CardActions sx={{ justifyContent: 'center' }}>
-          <a class="flex items-center mt-auto text-[13px] text-black" href="/company/blog1">
-            Read More
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M9 6l6 6l-6 6"></path>
-            </svg>
-        </a>
+            <Button variant="contained" color="primary" onClick={handleSelect}>
+              Read More
+            </Button>
           </CardActions>
         </Card>
-        
+
+        <Card sx={{ maxWidth: 345,borderRadius: '10%' }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://img.freepik.com/free-photo/online-message-blog-chat-communication-envelop-graphic-icon-concept_53876-139717.jpg"
+            alt="Model Image"
+          />
+          <div className="flex justify-between space-x-6 mb-3 text-black mt-4 mx-4">
+            <time dateTime="2024-01-06 21:52:42" className="text-sm">06 Jan</time>
+            <div className="border-b grow relative -top-2"></div>
+            <a className="text-sm" href="">Admin</a>
+          </div>
+
+          <CardContent sx={{ paddingTop: 0 }}>
+            <Typography gutterBottom variant="h5" component="div">
+              Blog Name:
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              context of the blog
+            </Typography>
+          </CardContent>
+          <CardActions sx={{ justifyContent: 'center' }}>
+            <Button variant="contained" color="primary" onClick={handleSelect}>
+              Read More
+            </Button>
+          </CardActions>
+        </Card>
       </div><br/><br/><br/>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
